2dc710e2852b869848e2877f0e4756b1a2fbc230
Sarah <kretaksson@gmail.com>
2023-03-31 13:44:36 -0700

    ___

--- react-app/src/components/playlists/PlaylistPage.js
+++ react-app/src/components/playlists/PlaylistPage.js
@@ -2,40 +2,44 @@ import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getSinglePlaylist } from "../../store/playlists";
+import { useHistory } from "react-router-dom";
 import SongRow from "../SongRow";
 import "./PlaylistPage.css";
 
 const PlaylistPage = () => {
     const { PlaylistId } = useParams();
+    const history = useHistory();
     const dispatch = useDispatch();
     const playlist = useSelector((state) => state.playlists.singlePlaylist);
     const songs = useSelector((state) => state.playlists.singlePlaylist.songs);
     const [errors, setErrors] = useState([]);
     const user = useSelector((state) => state.session.user);
+
     const addSongs = () => {
         history.push(`/playlists/add/${PlaylistId}`);
+    }
 
-        useEffect(() => {
-            console.log(playlist, "playlist in the playlist page");
-            dispatch(getSinglePlaylist(PlaylistId));
-        }, [dispatch, PlaylistId]);
+    useEffect(() => {
+        console.log(playlist, "playlist in the playlist page");
+        dispatch(getSinglePlaylist(PlaylistId));
+    }, [dispatch, PlaylistId]);
 
-        if (!playlist) {
-            return null;
-        }
+    if (!playlist) {
+        return null;
+    }
 
-        return (
-            <div className="PlaylistPage">
-                <div className="PlaylistPage__header">
-                    <h1>{playlist?.name}</h1>
-                    {playlist?.user && <p>Created by {playlist.user.username}</p>}
-                </div>
-                <button onClick={addSongs}>Add Songs</button>
-                {songs && Object.values(songs).map((song) => (
-                    <SongRow key={song.id} song={song} PlaylistId={PlaylistId} />
-                ))}
+    return (
+        <div className="PlaylistPage">
+            <div className="PlaylistPage__header">
+                <h1>{playlist?.name}</h1>
+                {playlist?.user && <p>Created by {playlist.user.username}</p>}
             </div>
-        );
-    };
+            <button onClick={addSongs}>Add Songs</button>
+            {songs && Object.values(songs).map((song) => (
+                <SongRow key={song.id} song={song} PlaylistId={PlaylistId} />
+            ))}
+        </div>
+    );
+};
 
-    export default PlaylistPage;
+export default PlaylistPage;
